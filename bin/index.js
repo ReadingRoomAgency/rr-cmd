10353ab01089d5a5b447b32f89cccf1adc1ddd94
Charlie Jackson <charlie@charliejackson.com>
2017-05-04 11:15:45 +0100

    Log version

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,27 @@
 import winston from 'winston';
+import commandLineArgs from 'command-line-args';
+import packageJson from '../package.json';
 
 winston.cli();
 
-winston.log('info', 'I am alive');
+const optionDefinitions = [
+  { name: 'version', alias: 'v', type: Boolean },
+  { name: 'debug', alias: 'd', type: Boolean },
+  { name: 'cmd', type: String, multiple: true, defaultOption: true },
+];
+
+const options = commandLineArgs(optionDefinitions);
+
+if (options.debug) {
+  winston.level = 'debug';
+}
+
+winston.log('debug', 'DEBUG ENABLED');
+winston.log('debug', 'Options', options);
+
+if (options.version || options.cmd[0] === 'version') {
+  winston.log('debug', 'Log version');
+  winston.log('info', `${packageJson.name} version: ${packageJson.version}`);
+}
+
+winston.log('debug', 'End of script');
